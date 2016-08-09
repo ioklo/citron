cb2b8c6c3846411e79b8fde4924f625d52511751
IOKLO <leesjung@gmail.com>
2016-08-09 22:41:12 +0900

    create new Sandbox

---
+++ Src/Sandbox.old/Program.cs
@@ -0,0 +1,49 @@
+﻿using Gum.Evaluator;
+using Gum.Lang.AbstractSyntax;
+using Gum.Translator.Text2AST;
+using Gum.Translator.Text2AST.Parsing;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Gum.Sandbox
+{
+    // Gum 개발 중 빠르게 실험해 보고 싶을때 쓰는 클래스
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string code = File.ReadAllText(@"..\..\Tests\00_Print.gum");
+
+            // preprocessing
+            code = Regex.Replace(code, "//.*$", " ", RegexOptions.Multiline);
+
+            var lexer = new Lexer(code);
+            var ast = Parser<FileUnit, FileUnitParser>.Parse(lexer);
+
+            // ASTEvaluator.Eval(ast);
+
+
+            // ASTPrinter.Print(ast);
+
+            // 실행기는..
+            // var evaluator = new Evaluator();
+
+            // 메타 데이터라고 표현하는 것이 좋은지?
+            // evaluator.BuildMetadata(fileUnit);
+
+            // First Pass: AST -> Metadata
+
+            // 두 패스로 실행을 합니다. 
+            // 1. 모든 파일들을 훑어서 메타데이터를 만들어내는 역할을 합니다.
+
+            // 2. 훑은 파일을 이제 실행하는 역할을 합니다.
+            
+            // fileUnit.Print();
+        }
+    }
+}
--- Src/Sandbox.old/Properties/AssemblyInfo.cs
+++ Src/Sandbox.old/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 어셈블리에 대한 일반 정보는 다음 특성 집합을 통해 
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
-[assembly: AssemblyTitle("Sandbox2")]
+[assembly: AssemblyTitle("Gum.Sandbox")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Sandbox2")]
+[assembly: AssemblyProduct("Gum.Sandbox")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
-[assembly: Guid("35418e13-7094-4dbe-bf10-5046fd1b054c")]
+[assembly: Guid("d0b6ae84-04f4-43ad-84d8-f15cfc1cc2ee")]
 
 // 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.
 //
