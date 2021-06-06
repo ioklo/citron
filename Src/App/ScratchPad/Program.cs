8d41847124260182a6eefe39e9bebdff39958df6
IOKLO <leesjung@gmail.com>
2021-06-06 10:28:54 +0900

    2021-06-06 10:28:54 daily

--- Src/App/ScratchPad/Program.cs
+++ Src/App/ScratchPad/Program.cs
@@ -31,7 +31,7 @@ namespace ScratchPad
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new Htt pClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             var host = builder.Build();
 
