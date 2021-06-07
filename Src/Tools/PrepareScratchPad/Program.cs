9a9fe5f42c6f37e88903c51d352dd94c8f90df22
IOKLO <leesjung@gmail.com>
2021-06-07 10:28:53 +0900

    2021-06-07 10:28:53 daily

---
+++ Src/Tools/PrepareScratchPad/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace PrepareScratchPad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var files = new List<string>();
+            files.Add("hello");
+            files.Add("world");
+            var text = JsonSerializer.Serialize(files);
+
+            Console.WriteLine(text);
+        }
+    }
+}
