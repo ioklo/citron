ad057cc2fddfb7dd52c8d737230e06b8812b405a
IOKLO <leesjung@gmail.com>
2025-10-14 14:35:28 +0900

    ImExpVisitable 작성

--- src/SyntaxIR0Translator/ImExp.h
+++ src/SyntaxIR0Translator/ImExp.h
@@ -367,5 +367,108 @@ public:
     void Accept(ImExpVisitor& visitor) override { visitor.Visit(this); }
 };
 
+template<class TFrom, class TVisitor>
+concept ImExpConvertibleToResultType = std::convertible_to<TFrom, typename std::remove_cvref_t<TVisitor>::ResultType>;
+
+// TResult타입은 &가 안되므로, reference_wrapper<TResult>를 쓰도록 합니다
+template<typename TVisitor, typename... TVisitorArgs>
+concept ImExpVisitable = requires(TVisitor&& v, TVisitorArgs&&... args) {
+    typename std::remove_cvref_t<TVisitor>::ResultType;
+
+    { v.Visit(std::declval<ImExp_Namespace*>(), std::forward<TVisitorArgs>(args)...) } -> ImExpConvertibleToResultType<TVisitor>;
+    { v.Visit(std::declval<ImExp_GlobalFuncs*>(), std::forward<TVisitorArgs>(args)...) } -> ImExpConvertibleToResultType<TVisitor>;
+    { v.Visit(std::declval<ImExp_TypeVar*>(), std::forward<TVisitorArgs>(args)...) } -> ImExpConvertibleToResultType<TVisitor>;
+    { v.Visit(std::declval<ImExp_Class*>(), std::forward<TVisitorArgs>(args)...) } -> ImExpConvertibleToResultType<TVisitor>;
+    { v.Visit(std::declval<ImExp_ClassFuncs*>(), std::forward<TVisitorArgs>(args)...) } -> ImExpConvertibleToResultType<TVisitor>;
+    { v.Visit(std::declval<ImExp_Struct*>(), std::forward<TVisitorArgs>(args)...) } -> ImExpConvertibleToResultType<TVisitor>;
+    { v.Visit(std::declval<ImExp_StructFuncs*>(), std::forward<TVisitorArgs>(args)...) } -> ImExpConvertibleToResultType<TVisitor>;
+    { v.Visit(std::declval<ImExp_Enum*>(), std::forward<TVisitorArgs>(args)...) } -> ImExpConvertibleToResultType<TVisitor>;
+    { v.Visit(std::declval<ImExp_EnumElem*>(), std::forward<TVisitorArgs>(args)...) } -> ImExpConvertibleToResultType<TVisitor>;
+    { v.Visit(std::declval<ImExp_ThisVar*>(), std::forward<TVisitorArgs>(args)...) } -> ImExpConvertibleToResultType<TVisitor>;
+    { v.Visit(std::declval<ImExp_LocalVar*>(), std::forward<TVisitorArgs>(args)...) } -> ImExpConvertibleToResultType<TVisitor>;
+    { v.Visit(std::declval<ImExp_LambdaVar*>(), std::forward<TVisitorArgs>(args)...) } -> ImExpConvertibleToResultType<TVisitor>;
+    { v.Visit(std::declval<ImExp_ClassVar*>(), std::forward<TVisitorArgs>(args)...) } -> ImExpConvertibleToResultType<TVisitor>;
+    { v.Visit(std::declval<ImExp_StructVar*>(), std::forward<TVisitorArgs>(args)...) } -> ImExpConvertibleToResultType<TVisitor>;
+    { v.Visit(std::declval<ImExp_EnumElemVar*>(), std::forward<TVisitorArgs>(args)...) } -> ImExpConvertibleToResultType<TVisitor>;
+    { v.Visit(std::declval<ImExp_ListIndexer*>(), std::forward<TVisitorArgs>(args)...) } -> ImExpConvertibleToResultType<TVisitor>;
+    { v.Visit(std::declval<ImExp_LocalDeref*>(), std::forward<TVisitorArgs>(args)...) } -> ImExpConvertibleToResultType<TVisitor>;
+    { v.Visit(std::declval<ImExp_BoxDeref*>(), std::forward<TVisitorArgs>(args)...) } -> ImExpConvertibleToResultType<TVisitor>;
+    { v.Visit(std::declval<ImExp_Else*>(), std::forward<TVisitorArgs>(args)...) } -> ImExpConvertibleToResultType<TVisitor>;
+};
+
+template<typename TVisitor, typename... TVisitorArgs> requires ImExpVisitable<TVisitor, TVisitorArgs...>
+decltype(auto) Accept(TVisitor&& v, ImExp* e, TVisitorArgs&&... args)
+{
+    using TResult = typename std::remove_cvref_t<TVisitor>::ResultType;
+
+    // using TResult = decltype(v.Visit(std::declval<MNamespaceDecl*>(), std::forward<U>(u)...));
+
+    // 계약 타입으로 변환(값/참조 정책을 Visit 시그니처가 결정)
+    auto caller = [&](auto* e) { return v.Visit(e, std::forward<TVisitorArgs>(args)...); };
+
+    if constexpr (std::is_void_v<TResult>)
+    {
+        struct Bridge : ImExpVisitor {
+            decltype(caller)& call;
+            Bridge(decltype(caller)& call) : call(call) {}
+            void Visit(ImExp_Namespace* imExp) override { call(imExp); }
+            void Visit(ImExp_GlobalFuncs* imExp) override { call(imExp); }
+            void Visit(ImExp_TypeVar* imExp) override { call(imExp); }
+            void Visit(ImExp_Class* imExp) override { call(imExp); }
+            void Visit(ImExp_ClassFuncs* imExp) override { call(imExp); }
+            void Visit(ImExp_Struct* imExp) override { call(imExp); }
+            void Visit(ImExp_StructFuncs* imExp) override { call(imExp); }
+            void Visit(ImExp_Enum* imExp) override { call(imExp); }
+            void Visit(ImExp_EnumElem* imExp) override { call(imExp); }
+            void Visit(ImExp_ThisVar* imExp) override { call(imExp); }
+            void Visit(ImExp_LocalVar* imExp) override { call(imExp); }
+            void Visit(ImExp_LambdaVar* imExp) override { call(imExp); }
+            void Visit(ImExp_ClassVar* imExp) override { call(imExp); }
+            void Visit(ImExp_StructVar* imExp) override { call(imExp); }
+            void Visit(ImExp_EnumElemVar* imExp) override { call(imExp); }
+            void Visit(ImExp_ListIndexer* imExp) override { call(imExp); }
+            void Visit(ImExp_LocalDeref* imExp) override { call(imExp); }
+            void Visit(ImExp_BoxDeref* imExp) override { call(imExp); }
+            void Visit(ImExp_Else* imExp) override { call(imExp); }
+        };
+
+        Bridge bridge{caller};
+        e->Accept(bridge);
+    }
+    else
+    {
+        struct Bridge : ImExpVisitor {
+            decltype(caller)& call;
+            std::optional<TResult> result{};
+            Bridge(decltype(caller)& call) : call(call) {}
+
+            void Visit(ImExp_Namespace* imExp) override { result.emplace(call(imExp)); }
+            void Visit(ImExp_GlobalFuncs* imExp) override { result.emplace(call(imExp)); }
+            void Visit(ImExp_TypeVar* imExp) override { result.emplace(call(imExp)); }
+            void Visit(ImExp_Class* imExp) override { result.emplace(call(imExp)); }
+            void Visit(ImExp_ClassFuncs* imExp) override { result.emplace(call(imExp)); }
+            void Visit(ImExp_Struct* imExp) override { result.emplace(call(imExp)); }
+            void Visit(ImExp_StructFuncs* imExp) override { result.emplace(call(imExp)); }
+            void Visit(ImExp_Enum* imExp) override { result.emplace(call(imExp)); }
+            void Visit(ImExp_EnumElem* imExp) override { result.emplace(call(imExp)); }
+            void Visit(ImExp_ThisVar* imExp) override { result.emplace(call(imExp)); }
+            void Visit(ImExp_LocalVar* imExp) override { result.emplace(call(imExp)); }
+            void Visit(ImExp_LambdaVar* imExp) override { result.emplace(call(imExp)); }
+            void Visit(ImExp_ClassVar* imExp) override { result.emplace(call(imExp)); }
+            void Visit(ImExp_StructVar* imExp) override { result.emplace(call(imExp)); }
+            void Visit(ImExp_EnumElemVar* imExp) override { result.emplace(call(imExp)); }
+            void Visit(ImExp_ListIndexer* imExp) override { result.emplace(call(imExp)); }
+            void Visit(ImExp_LocalDeref* imExp) override { result.emplace(call(imExp)); }
+            void Visit(ImExp_BoxDeref* imExp) override { result.emplace(call(imExp)); }
+            void Visit(ImExp_Else* imExp) override { result.emplace(call(imExp)); }
+        };
+
+        Bridge bridge{caller};
+        e->Accept(bridge);
+
+        return *bridge.result;
+    }
+}
+
 } // namespace SyntaxIR0Translator
 } // namespace Citron
